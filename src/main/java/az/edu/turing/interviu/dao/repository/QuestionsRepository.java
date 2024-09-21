9880baa9ae31a1166e8ed53b988140e33ed912f6
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-09-21 18:47:27 +0400

    repository added

---
+++ src/main/java/az/edu/turing/interviu/dao/repository/QuestionsRepository.java
@@ -0,0 +1,13 @@
+package az.edu.turing.interviu.dao.repository;
+
+import az.edu.turing.interviu.dao.entity.QuestionsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionsRepository extends JpaRepository<QuestionsEntity, Long> {
+    List<QuestionsEntity> findByCategory(String category);
+    void saveQuestions(QuestionsEntity questionsEntity);
+}
