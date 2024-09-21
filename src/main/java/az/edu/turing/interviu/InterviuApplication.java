2244e0b9bf99bd23bdd4b8717b675af59b1e034e
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-09-21 18:50:58 +0400

    InterviuApplication

---
+++ src/main/java/az/edu/turing/interviu/InterviuApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.interviu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviuApplication.class, args);
+    }
+
+}
