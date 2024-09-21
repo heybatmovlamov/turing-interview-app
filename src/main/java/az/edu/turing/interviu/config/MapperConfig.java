d475b182aee57613db8da3aa23c2762e976f58e7
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-09-21 18:50:38 +0400

    enums and MapperConfig added

---
+++ src/main/java/az/edu/turing/interviu/config/MapperConfig.java
@@ -0,0 +1,20 @@
+package az.edu.turing.interviu.config;
+
+import az.edu.turing.interviu.mapper.QuestionsMapper;
+import az.edu.turing.interviu.mapper.UserMapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public UserMapper userMapper() {
+        return Mappers.getMapper(UserMapper.class);
+    }
+    @Bean
+    public QuestionsMapper questionsMapper() {
+        return Mappers.getMapper(QuestionsMapper.class);
+    }
+}
---
+++ src/main/java/az/edu/turing/interviu/model/enums/CategoryEnums.java
@@ -0,0 +1,11 @@
+package az.edu.turing.interviu.model.enums;
+
+public enum CategoryEnums {
+    BACKEND,
+    FRONTEND,
+    UX_UI,
+    PROJECT_MANAGEMENT,
+    PRODUCT_OWNER,
+    DATA_SCIENCE,
+    DATA_ANALYTICS
+}
